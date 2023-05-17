851af17b2c6c5e92239bd5cdc91ab43525804cdf
Juanmat1317 <89482278+Juanmat1317@users.noreply.github.com>
2023-05-16 23:20:04 -0500

    minga-02 (#1)

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 import express from "express";
 import "dotenv/config.js"
+import "./config/database.js"
 
 const server = express(); // se crea el servidor
 const PORT = process.env.PORT || 8080;// se define el puerto
-
 const ready = () => console.log('server ready on port: ' + PORT);
+
+//middlewarwes
+server.use(express.json())
+server.use(express.urlencoded({extended: false}))
+
+//router + server
 server.listen(PORT,ready)// iniciar servidor
 
 console.log(process.env.NODE_ENV);
